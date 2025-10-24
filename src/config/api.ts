66136b8bd96d493aae0359a31898df78cf9d98a5
seynabou thiandoum <thiandoumzeynab8@gmail.com>
2025-10-24 15:58:18 +0100

    Add de la lien en dur

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,13 +1,24 @@
 import axios from 'axios';
+const getApiUrl = () => {
+  // En production sur Render
+  if (window.location.hostname !== 'localhost') {
+    return 'https://afrizar-back-end.onrender.com';
+  }
+  // En développement local
+  return 'http://localhost:8080';
+};
 
-console.log("BASE_URL:", process.env.REACT_APP_API_URL); // <- Ajoute ici
 // Configuration de l'API
 export const API_CONFIG = {
-  BASE_URL: process.env.REACT_APP_API_URL,
+  BASE_URL: getApiUrl(),
+  // BASE_URL: REACT_APP_API_URL,
   // BASE_URL: 'http://localhost:8080',
   TIMEOUT: 10000, // 10 secondes
   RETRY_ATTEMPTS: 3,
 };
+
+console.log("BASE_URL:", API_CONFIG.BASE_URL); // <- Ajoute ici
+
 
 // Instance axios configurée
 export const api = axios.create({
