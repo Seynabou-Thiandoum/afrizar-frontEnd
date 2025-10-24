e291b8299f45d0b10c57cc8a3d99bef7675ffdd0
seynabou thiandoum <thiandoumzeynab8@gmail.com>
2025-10-24 12:49:44 +0100

    Add REACT_APP_API_URL

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Configuration de l'API
 export const API_CONFIG = {
-  BASE_URL: 'http://localhost:8080',
+  BASE_URL: process.env.REACT_APP_API_URL,
+  // BASE_URL: 'http://localhost:8080',
   TIMEOUT: 10000, // 10 secondes
   RETRY_ATTEMPTS: 3,
 };
